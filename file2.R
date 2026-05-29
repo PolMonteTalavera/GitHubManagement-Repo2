81d629eb98c494c00ebf42f62a4c57fc38e8e4ab
PolMonteTalavera <pol.monte@datancia.com>
2026-05-29 13:09:59 +0200

    Commit 5.

--- file2.R
+++ file2.R
@@ -3,6 +3,10 @@ calcular_mitjana <- function(df) {
   mean(df$valor, na.rm = TRUE)
 }
 
+calcular_variancia <- function(df) {
+  var(df$valor, na.rm = TRUE)
+}
+
 print(paste0("Mitjana calculada correctament"))
 
 calcular_mediana <- function(df) {
